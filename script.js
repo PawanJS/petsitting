63c8c572b8f716807ec4861582a50ff3eb2ca5cb
PawanJS <contactpawanjs@gmail.com>
2020-12-09 10:04:14 +0530

    PETSITTING website

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function calcAge(birthYear) {
+  return 2020 - birthYear;
+}
+
+const calcAge = function (birthYear) {
+  return 2020 - birthYear;
+};
+
+const calcAge = (birthYear) => 2020 - birthYear;
